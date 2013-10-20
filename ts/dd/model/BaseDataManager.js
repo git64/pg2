fd89974788e34df0d87e7ba20b92058fc1b12155
Nils <nils@ezisonline.de>
2013-10-20 02:07:11 +0200

    test

--- ts/dd/model/BaseDataManager.js
+++ ts/dd/model/BaseDataManager.js
@@ -3,7 +3,8 @@ var dd;
     (function (model) {
         var BaseDataManager = (function () {
             function BaseDataManager() {
-                var hello = $("<div>HELLO WORLD</div>");
+                var hello = $("<div id='tButton' style='height:200px;width:100%;background-color:#E0E0E0;'>HELLO WORLD</div>");
+
                 $("body").append(hello);
             }
             return BaseDataManager;
@@ -15,4 +16,8 @@ var dd;
 
 $(window).ready(function () {
     new dd.model.BaseDataManager();
+
+    $("#tButton").click(function () {
+        $("#tButton").hide();
+    });
 });
--- ts/dd/model/BaseDataManager.ts
+++ ts/dd/model/BaseDataManager.ts
@@ -1,12 +1,22 @@
 module dd.model {
     export class BaseDataManager {
         constructor() {
-            var hello = $("<div>HELLO WORLD</div>");
+
+            var hello = $("<div id='tButton' style='height:200px;width:100%;background-color:#E0E0E0;'>HELLO WORLD</div>");
+
             $("body").append(hello);
+
         }
     }
 }
 
 $(window).ready(function(): void {
     new dd.model.BaseDataManager();
+
+
+    $("#tButton").click(function() {
+        $("#tButton").hide();
+    });
+
+
 });
