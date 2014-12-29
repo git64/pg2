1b20e07d2fe31d1f256b18feccdf4996dc1d9252
Nils <nils@ezisonline.de>
2014-12-29 14:57:04 +0100

    firstSteps

--- references.js
+++ references.js
@@ -1,3 +1,2 @@
 /// <reference path='ts/definitions/jquery-1.10.2.d.ts' />
 /// <reference path='ts/definitions/phonegap.d.ts' />
-/// <reference path='ts/definitions/youtube.d.ts' />
--- references.ts
+++ references.ts
@@ -1,6 +1,6 @@
 /// <reference path='ts/definitions/jquery-1.10.2.d.ts' />
 /// <reference path='ts/definitions/phonegap.d.ts' />
-/// <reference path='ts/definitions/youtube.d.ts' />
+
 
 /// <reference path="ts/dd/core/Signal.ts" />
 /// <reference path="ts/dd/model/BaseDataManager.ts" />
@@ -9,7 +9,7 @@
 /// <reference path="ts/dd/ui/Icon.ts" />
 /// <reference path="ts/dd/ui/media/BaseMedia.ts" />
 /// <reference path="ts/dd/ui/media/VideoPlayer.ts" />
-/// <reference path="ts/dd/ui/media/YouTubePlayer.ts" />
+
 /// <reference path="ts/dd/ui/info/BaseInfo.ts" />
 /// <reference path="ts/dd/ui/info/Notification.ts" />
 /// <reference path="ts/dd/ui/info/Toast.ts" />
