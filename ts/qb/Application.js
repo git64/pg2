99b9ba91f2ba4271c21180462f0b1511860c8bcf
Nils <nils@ezisonline.de>
2015-01-22 23:28:50 +0100

    Signed-off-by: Nils <nils@ezisonline.de>

--- ts/qb/Application.js
+++ ts/qb/Application.js
@@ -16,7 +16,8 @@ var dd;
         Application.prototype.handleDeviceReady = function () {
             console.log("READY");
             $(".open-test-panel").append("<div id='test' style='border:thin solid #EEAD0E;height: auto; position:absolute;width: 70%;margin: auto;'></div>");
-            $("#test").append("<div style='border:thin solid #EEAD0E; position:absolute; width: 70%;height: auto;top: 20%; right: 0px; bottom: 20%; left: 0px; margin: auto;text-align: center;'>stackoverflow<input type='text' list='browsers' style='width: 80%; top: 20%; margin:auto;'><datalist id='browsers'><option value='Internet Explorer'><option value='Firefox'></datalist></div>");
+            $("#test").append("<input type='text' list='browsers' style='width: 80%; top: 20%; margin:auto;'><datalist id='browsers'><option value='Internet Explorer'><option value='Firefox'></datalist>");
+            $("#test").append("<div style='border:thin solid #EEAD0E; position:absolute; width: 70%;height: auto;top: 20%; right: 0px; bottom: 20%; left: 0px; margin: auto;text-align: center;'>stackoverflow</div>");
             $("#test").append("<div style='border:thin solid #EEAD0E; width:70%; margin:10px auto;'>Ngoverflow</div>");
             //$(".open-test-panel").append("<input type='text' list='browsers' style='position: absolute; width: 70%; top: 20%; margin:auto;'><datalist id='browsers'><option value='Internet Explorer'><option value='Firefox'></datalist>");
             //$(".open-test-panel").append("<input type='text' list='browsers' style='position: absolute; width: 70%; top: 60%; margin:auto;'><datalist id='browsers'><option value='Internet Explorer'><option value='Firefox'></datalist>");
