d278c60eedf5013408667db96d6657f7d05e08b7
Nils <nils@ezisonline.de>
2015-04-11 23:15:15 +0200

    Signed-off-by: Nils <nils@ezisonline.de>

--- ts/qb/Application.ts
+++ ts/qb/Application.ts
@@ -29,9 +29,9 @@ module dd {
             
             //#$(".open-test-panel").append("<div id='test' style='border:thin solid #EEAD0E;height: auto; position:absolute;width: 70%;margin: auto;'></div>");
             
-            $(".open-test-panel").append("<div id='test2' style='border:thin solid #EEAD0E; position:absolute; width: 70%;height: auto;top: 20%; right: 0px; bottom: 20%; left: 0px; margin: auto;text-align: center;'>stackoverflow</div>");
+           // $(".open-test-panel").append("<div id='test2' style='border:thin solid #EEAD0E; position:absolute; width: 70%;height: auto;top: 20%; right: 0px; bottom: 20%; left: 0px; margin: auto;text-align: center;'>stackoverflow</div>");
             
-            $("#test2").append("<input type='text' list='browsers' style='width: 80%; top: 20%; margin:auto;'><datalist id='browsers'><option value='Internet Explorer'><option value='Firefox'></datalist>")
+            $(".open-test-panel").append("<input type='text' list='browsers' style='width: 80%; top: 20%; margin:auto;'><datalist id='browsers'><option value='Internet Explorer'><option value='Firefox'></datalist>")
             
             //$("#test").append("<div style='border:thin solid #EEAD0E; width:70%; margin:10px auto;'>Ngoverflow</div>");
             
